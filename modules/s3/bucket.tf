409b61b9b437b39a0219c53033a571730ceb37fc
Tang Zhen <cloudztang@gmail.com>
2024-08-31 08:49:22 +0800

    [HF-43][Zhen Tang]feat: apply the encryption and block public acl for the s3

--- modules/s3/bucket.tf
+++ modules/s3/bucket.tf
@@ -10,32 +10,31 @@ resource "aws_s3_bucket_versioning" "bucket_versioning" {
   }
 }
 
-# TODO: Can not apply this public access block and encryption because right now the file be uploaded by web directly through the presigned url
-# resource "aws_kms_key" "s3_encryption_key" {
-#   description             = "KMS key for encrypting S3 bucket data"
-#   enable_key_rotation     = true
-#   deletion_window_in_days = 30
-# }
-#
-# resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
-#   bucket = aws_s3_bucket.backend_media_bucket.id
-#
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm     = "aws:kms"
-#       kms_master_key_id = aws_kms_key.s3_encryption_key.id
-#     }
-#   }
-# }
+resource "aws_kms_key" "s3_encryption_key" {
+  description             = "KMS key for encrypting S3 bucket data"
+  enable_key_rotation     = true
+  deletion_window_in_days = 30
+}
 
-# resource "aws_s3_bucket_public_access_block" "bucket_public_access_block" {
-#   bucket = aws_s3_bucket.backend_media_bucket.id
-#
-#   block_public_acls       = true
-#   ignore_public_acls      = true
-#   block_public_policy     = true
-#   restrict_public_buckets = true
-# }
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
+  bucket = aws_s3_bucket.backend_media_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.s3_encryption_key.id
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_public_access_block" {
+  bucket = aws_s3_bucket.backend_media_bucket.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
 
 resource "aws_s3_bucket_cors_configuration" "bucket_cors" {
   bucket = aws_s3_bucket.backend_media_bucket.id
